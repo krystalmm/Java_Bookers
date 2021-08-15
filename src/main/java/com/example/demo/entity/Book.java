5c42b14896c1bc5af85c92db4b65128fbc059663
krystalmm <taeyeon_aina@live.jp>
2021-08-15 09:36:01 +0000

    [add] anotation to ID

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import lombok.Data;
 public class Book {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookId;
 
     private String title;
